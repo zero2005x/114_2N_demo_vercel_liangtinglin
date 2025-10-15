d880d1d08fc836c81d517ba01923ba0b3a37a2cc
zero2005x <zero2005x@gmail.com>
2025-10-15 20:37:55 +0800

    Add Tailwind CSS integration and refactor components for improved styling

--- src/page/HomePage_14.jsx
+++ src/page/HomePage_14.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const HomePage_14 = () => {
   return (
-    <div>
-     <h1>HomePage_14</h1>
-     <h2>liangtinglin, 913410014</h2>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-2xl text-indigo-700 mb-6">HomePage_14</h1>
+      <h2 className="text-xl mb-4 text-purple-700">liangtinglin, 913410014</h2>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage_14;
